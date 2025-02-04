748fe3ea5d7224c1d6c866309a32a35926b63bb6
fahadbgnr <aminulislamfahad1@gmail.com>
2025-02-05 00:37:07 +0600

    math for js

---
+++ math.js
@@ -0,0 +1,26 @@
+/***********************************************************
+ *                 MathNeeded for programming     
+ * *********************************************************
+ * 1.number: +ve, -ve, 0 [50, -9, o]
+ * 2.integer: 2, 88, [odd, even]: 2, 4, 897
+ * 3.float: 5.6, 2.2
+ * 4.operation: +, -, *, /
+ * 5.more operation: 5%, 50 taka er 10% koto?
+ * 6.percentage: %, [ ]
+ * 7.sum: 14, 18, 16
+ * 8.avg: 14, 18, 16
+ * 9.max/min: 14, 18, 16
+ * 10.series:
+ * 11.area: Triangle
+ * */ 
+* 1.number: +ve, -ve, 0 [50, -9, o]
+ * 2.integer: 2, 88, [odd, even]: 2, 4, 897
+ * 3.float: 5.6, 2.2
+ * 4.operation: +, -, *, /
+ * 5.more operation: 5%, 50 taka er 10% koto?
+ * 6.percentage: %, [ ]
+ * 7.sum: 14, 18, 16
+ * 8.avg: 14, 18, 16
+ * 9.max/min: 14, 18, 16
+ * 10.series:
+ * 11.area: Triangle
