ffd043451b5dab672d40ca22df6b4e664158d798
fahadbgnr <aminulislamfahad1@gmail.com>
2025-02-05 01:36:01 +0600

    first night at js

---
+++ type.js
@@ -0,0 +1,16 @@
+// string 
+var name= "Fahad";
+console.log(name);
+
+var address = 'dhaka';
+ var country = "bangladesh";
+
+//  boolean
+var isRich = true;
+var passed = false;
+console.log(isRich);
+console.log(typeof passed);
+
+// number
+var price = 555;
+console.log(typeof price);
---
+++ variable.js
@@ -0,0 +1,15 @@
+/*******************************************
+ *    variable ==> very +able
+ * 5 things to declare a variable
+ * */ 
+var weight = 38 ; 
+var price = 38 ;
+var year = 1999 ;
+var money = 2005 ;
+money = 500;  
+/* kono kiser man pore change korar jonno var lekha lagbe na */ 
+
+
+var age;
+console.log(price);
+console.log(money);
